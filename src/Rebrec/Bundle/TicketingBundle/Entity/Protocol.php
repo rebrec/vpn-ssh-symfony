0d4c16dfae34ee17a943d60a6b01ef50e9f687f3
Francois Laplaud <elfrancesco+git@gmail.com>
2014-08-08 23:13:30 +0200

    added entities tunnel and protocol

---
+++ src/Rebrec/Bundle/TicketingBundle/Entity/Protocol.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Rebrec\Bundle\TicketingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Protocol
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Protocol
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=20)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Protocol
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ src/Rebrec/Bundle/TicketingBundle/Entity/Tunnel.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Rebrec\Bundle\TicketingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tunnel
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Tunnel
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Name", type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="HostIp", type="string", length=16)
+     */
+    private $hostIp;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="HostPort", type="integer")
+     */
+    private $hostPort;
+
+    /**
+     * @var string
+     * 
+     * @ORM\OneToMany(targetEntity="Protocol", mappedBy="Name")
+     */
+    private $protocol;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Tunnel
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set hostIp
+     *
+     * @param string $hostIp
+     * @return Tunnel
+     */
+    public function setHostIp($hostIp)
+    {
+        $this->hostIp = $hostIp;
+
+        return $this;
+    }
+
+    /**
+     * Get hostIp
+     *
+     * @return string 
+     */
+    public function getHostIp()
+    {
+        return $this->hostIp;
+    }
+
+    /**
+     * Set hostPort
+     *
+     * @param integer $hostPort
+     * @return Tunnel
+     */
+    public function setHostPort($hostPort)
+    {
+        $this->hostPort = $hostPort;
+
+        return $this;
+    }
+
+    /**
+     * Get hostPort
+     *
+     * @return integer 
+     */
+    public function getHostPort()
+    {
+        return $this->hostPort;
+    }
+
+    /**
+     * Set protocol
+     *
+     * @param string $protocol
+     * @return Tunnel
+     */
+    public function setProtocol($protocol)
+    {
+        $this->protocol = $protocol;
+
+        return $this;
+    }
+
+    /**
+     * Get protocol
+     *
+     * @return string 
+     */
+    public function getProtocol()
+    {
+        return $this->protocol;
+    }
+}
