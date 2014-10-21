2ae22d9efb23df968b9403f046ab58adcf17a3aa
Francois Laplaud <elfrancesco+git@gmail.com>
2014-10-21 17:32:03 +0200

    Authentication almost Working

---
+++ src/Rebrec/Bundle/VPNSSHBundle/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rebrec\Bundle\VPNSSHBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ */
+class User extends BaseUser // necessary to manage user with FOSUserBundle
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
